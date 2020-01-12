31f2028affd16d7a212c42a39e2e4cbeca3b4f9c
Rayed Bin Wahed <rayed@rayanalytics.org>
2020-01-12 15:47:18 +0600

    Replace activity code with actual value

--- util.py
+++ util.py
@@ -4,14 +4,25 @@ import numpy as np
 import pandas as pd
 
 DATA_DIR = 'data'
-RAW_DATA_DIR = 'wisdm_dataset/raw/watch'
-COLUMNS = ['subject_id', 'activity_code', 'timestamp']
+RAW_DATA_DIR = 'wisdm-dataset/raw/watch'
+ACTIVITY_CODE_PATH = 'wisdm-dataset/activity_key.txt'
+
+COLUMNS = ['subject_id', 'activity', 'timestamp']
 
 def read_sensor_data(sensor):
     if os.path.exists(os.path.join(DATA_DIR, sensor + '.csv')):
         return pd.read_csv(os.path.join(DATA_DIR, sensor + '.csv'))
     files = sorted(glob.glob(os.path.join(os.path.join(RAW_DATA_DIR, sensor), '*.txt')))
     df = pd.concat([pd.read_csv(file, header=None) for file in files], ignore_index=True)
-    df[5] = df[5].str.replace(';', '').astype(float)
-    df.columns = COLUMNS + [sensor + '-' + axis for axis in 'xyz']
+    df[5] = df[5].str.replace(';', '', regex=False).astype(float)
+    df.columns = COLUMNS + [sensor + '_' + axis for axis in 'xyz']
     return df
+
+def get_activity_map():
+    activity_map = {}
+    with open(ACTIVITY_CODE_PATH) as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        tokens = line.split()
+        activity_map[tokens[2]] = tokens[0]
+    return activity_map
