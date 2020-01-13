349cd26d0167b917357addf91ec0c481f370c794
Rayed Bin Wahed <rayed@therapservices.net>
2020-01-13 13:43:16 +0600

    Drop duplicates from dataframe

--- util.py
+++ util.py
@@ -10,10 +10,12 @@ ACTIVITY_CODE_PATH = 'wisdm-dataset/activity_key.txt'
 COLUMNS = ['subject_id', 'activity', 'timestamp']
 
 def read_sensor_data(sensor):
-    if os.path.exists(os.path.join(DATA_DIR, sensor + '.csv')):
-        return pd.read_csv(os.path.join(DATA_DIR, sensor + '.csv'))
+#     if os.path.exists(os.path.join(DATA_DIR, sensor + '.csv')):
+#         return pd.read_csv(os.path.join(DATA_DIR, sensor + '.csv'))
     files = sorted(glob.glob(os.path.join(os.path.join(RAW_DATA_DIR, sensor), '*.txt')))
-    df = pd.concat([pd.read_csv(file, header=None) for file in files], ignore_index=True)
+    df = pd.concat([pd.read_csv(file, header=None) for file in files], axis=0, ignore_index=True)
+    df.dropna(axis=0, inplace=True)
+    df.drop_duplicates(inplace=True)
     df[5] = df[5].str.replace(';', '', regex=False).astype(float)
     df.columns = COLUMNS + [sensor + '_' + axis for axis in 'xyz']
     return df
